54b36d03d1741b0f0d6e07d027813b87758eb05f
Amin <amin.haya-samodi@etu.univ-grenoble-alpes.fr>
2024-05-03 10:19:09 +0200

    Factorisation into incr and decr func

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -12,8 +12,8 @@ public class Player{
         pyramid = new Pyramid(i);
         side = new ArrayList<>();
     }
-    public void addSide(Cube c){
-        side.add(c);
+
+    public void increment(Cube c){
         switch (c) {
             case Noir:
                 noir++;
@@ -41,74 +41,7 @@ public class Player{
         }
     }
 
-    public boolean lost(){
-        return loss;
-    }
-    public Cube get(int x, int y){
-        return pyramid.get(x, y);
-    }
-
-    public Cube getSide(int x){
-        return side.get(x);
-    } 
-
-    public void set(int x, int y, Cube c){
-        Cube cube = get(x, y);
-        switch (cube) {
-            case Noir:
-                noir--;
-                break;
-            case Bleu:
-                bleu--;
-                break;
-            case Blanc:
-                blanc--;
-                break;
-            case Rouge:
-                rouge--;
-                break;
-            case Jaune:
-                jaune--;
-                break;
-            case Vert:
-                vert--;
-                break;
-            case Neutre:
-                neutre--;
-                break;
-            default:
-                break;
-        }
-        pyramid.set(x,y,c);
-        switch (c) {
-            case Noir:
-                noir++;
-                break;
-            case Bleu:
-                bleu++;
-                break;
-            case Blanc:
-                blanc++;
-                break;
-            case Rouge:
-                rouge++;
-                break;
-            case Jaune:
-                jaune++;
-                break;
-            case Vert:
-                vert++;
-                break;
-            case Neutre:
-                neutre++;
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void removeSide(int x){
-        side.remove(x);
+    public void decrement(Cube c){
         switch (side.get(x)) {
             case Noir:
                 noir--;
@@ -134,6 +67,34 @@ public class Player{
             default:
                 break;
         }
+    }
+
+    public void addSide(Cube c){
+        side.add(c);
+        increment(c);
+    }
+
+    public boolean lost(){
+        return loss;
+    }
+    public Cube get(int x, int y){
+        return pyramid.get(x, y);
+    }
+
+    public Cube getSide(int x){
+        return side.get(x);
+    } 
+
+    public void set(int x, int y, Cube c){
+        Cube cube = get(x, y);
+        decrement(cube);
+        pyramid.set(x,y,c);
+        increment(c);
+    }
+
+    public void removeSide(int x){
+        side.remove(x);
+        decrement(side.get(x));
     }
 
     public Pyramid getPyramid(){
