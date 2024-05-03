eedc06ded9bf3ecc5d0621e91fb37cf786cab7e3
Amin <amin.haya-samodi@etu.univ-grenoble-alpes.fr>
2024-05-03 13:52:57 +0200

    Size dans Pyramid

--- src/Model/Pyramid.java
+++ src/Model/Pyramid.java
@@ -2,15 +2,20 @@ package Model;
 
 public class Pyramid {
     Cube[][] pyramid;
-
+    int size;
 
     Pyramid(int i){
         pyramid = new Cube[i][i];
+        size = i;
         for (int x=0; x<i; x++ ){
             for (int y=0; y<i; y++ ){
                 pyramid[x][y] = Cube.Vide;
             }
         }
+    }
+
+    public int getsize(){
+        return size;
     }
 
     public Cube get(int x, int y){
